7f173d2ba80392045f9d2123d71b88c7ec68182c
Abdullox <maxammatovabdullox27@gmail.com>
2024-01-09 15:01:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// console.log("Maxammatov Abdullox Akramxoja ogli")
+// alert("salom siz loterayada yutdingiz")/// okm tugma bor va ogohlantirish uchun ishlatiladi
+// confirm("men rostdan yutdimmi?") //ok va cancel tugmasi bor va alert bilan deyarli bir xil
+// console.log("salom") //consolda chiqadi
+// document.write("bolajonlar") // htmld malumot chiqarish uchun ishlatiladi
+// var ism="Maxammatov Abdullox";
+// console.log(ism);
+// var age=21;
+// var myChildName="Abdulloh";
+// var myStudyCenter="SoffStudy";
+// console.log(age);
+// console.log(myChildName);
+// console.log(myStudyCenter);
+// var let const
+// reaeclare = 'qayta ozgaruvchi yaratish;
+// // reassign = 'ozgaruvchi qiymatini ozgartirish
+// var age=12;
+//  age=21;
+// let ism="Abdulloh";
+// ism="Abdullox";
+//  console.log(ism);
+// var left const
+// varda redeclere va ressign ishlaydi
+// letda ressign ishlatsa bo'ladi
+// // const mumkin emas
+//             Data  type
+// primitive               non-primitive
+// Number                  Object            
+// String
+// null
+// undefined
+// Boolean true false qaytaradi
+// BigInt
+// Symbol(unique)
+// xotirada saqlanish haqida
+// #Number istalgan raqam qabul qiladi
+// let age=12;
+// let son=100000000000;
+// console.log(Number, MAX_SAFE_INTEGER);
+
+// //String
+// "" '' ``//
+// let pepsi='';///null
+
+// console.log(pepsi);
---
+++ main1.js
@@ -0,0 +1,69 @@
+let FirstName = prompt("Ismingizni nima?");
+let LastName = prompt("Familiyangizni nima?"); 
+let Age = prompt("Necha yoshdasiz?");
+let Address = prompt("Yashash manzilingiz qayerda?");
+let PhoneNumber = prompt("Telfon nomeringizni nechchi?");
+let StudyCenter = prompt("O'qish joyingiz qayerda?");
+let Working = prompt("Dasturchimisiz?");
+let Email = prompt("E-mailgiz bormi?");
+let Hobby = prompt("Hobbyingiz nima? yoki nimaga qiziqasiz?")
+let CarName = prompt("Qaysi mashina yoqadi?");
+
+
+
+
+
+// let FirstName = prompt("Ismingizni kiriting", "Abdullox");
+// let LastName = prompt("Familiyangizni kiriting", "Maxammatov"); 
+// let Age = prompt("Yoshingizni kiriting", "21yoshdaman");
+// let Address = prompt("Yashash manzilingizni kiriting", "Yunusobod tumani Shaxriston 83a 57-xonadon");
+// let PhoneNumber = prompt("Telfon nomeringizni kiriting", "+998945037807");
+// let StudyCenter = prompt("SoffStudy","Guruh005");
+// let Working = prompt("Programmist","FrontEnd");
+// let Email = prompt("maxammatovabdullox27@gmail.com","E-mail account");
+// let Hobby = prompt("Shaxmat shashka va Web dasturlash", "Qiziqishingiz")
+// let CarName = prompt("Gentra","Uzbekiston mashinalari");
+
+
+
+
+
+
+// let Ismimgiz ='Abdullox'
+// console.log(Ismimgiz);
+// let Familiyangiz ='Maxammatov'
+// console.log(Familiyangiz);
+// let OtasiningizniIsmi ='Akramxoja ogli'
+// console.log(OtasiningizniIsmi);
+// let Yoshingiz ='21yosh'
+// console.log(Yoshingiz);
+// let TelfonNomeringiz ='+998(94) 503-78-07'
+// console.log(TelfonNomeringiz);
+// let JSHSHIR ='51408026520045'
+// console.log(JSHSHIR);
+// let PaspportingizSeriyasi ='AC0757839'
+// console.log(PaspportingizSeriyasi);
+// let Qiziqishingiz ='Dasturlash'
+// console.log(Qiziqishingiz);
+// let YoqtirganMashinanigiz ='Rolls Royce'
+// console.log(YoqtirganMashinanigiz);
+// let Email ='maxammatovabdullox27@gmail.com'
+// console.log(Email);
+
+
+
+
+
+// alert('Tizimdagi nososzlikni sozlash')
+// alert('Yangilanish mavjud')
+// alert('Yangilanishni urnatish')
+// alert('Bush fayllarni uchirib yuborish')
+// alert('Kesh fayllarnin uchirib yuborish')
+// alert('Telfoningizni optimizatsiya holatiga keltirish')
+// alert('Telfoningizni quvvati 5%qoldi')
+// alert('Telfoningizni quvvati tuldi')
+// alert('Ogohlantirish!')
+// alert('Dars yakullandi')
+
+
+
